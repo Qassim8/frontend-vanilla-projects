4e0d47ef5420e959a664d7a2bfffd6c423686203
qassim8 <mohammedalgasim123@gmail.com>
2025-12-14 13:50:35 +0200

    Add Show & Hide Element Logic

--- 00-dom-playground/main.js
+++ 00-dom-playground/main.js
@@ -17,3 +17,34 @@ const dicrementHandler = () => {
   if (counterValue > 0) counterValue--;
   return (counter.textContent = counterValue);
 };
+///////////////////////////////////////////////////
+// Show & Hide Element Logic
+
+let visibiltyHandlerBtn = document.querySelector(".visibilty-handler");
+let text = document.querySelector(".visible");
+
+visibiltyHandlerBtn.addEventListener("click", (e) => {
+  visibiltyHandler(e);
+});
+
+const visibiltyHandler = (e) => {
+  if (text.classList.contains("visible-text")) {
+    handleInvisible(e);
+  } else {
+    handleVisible(e);
+  }
+};
+
+const handleVisible = (e) => {
+  e.target.textContent = "Hide Text";
+  text.classList.remove("invisible-text");
+  text.classList.add("visible-text");
+};
+const handleInvisible = (e) => {
+  e.target.textContent = "Show Text";
+  text.classList.remove("visible-text");
+  text.classList.add("invisible-text");
+};
+
+/////////////////////////////////////////////////////////
+// Show & Hide Tabs Logic
